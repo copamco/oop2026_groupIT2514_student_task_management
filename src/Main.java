92d394f39a106cf6904cc5e524fab37c9c2347ef
copamco <spanocyerasyl@gmail.com>
2026-02-04 11:23:16 +0500

    Yerassyl Spanov - fix Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
 import static db.connect.*;
 
 import db.DatabaseConnection;
+import entities.Project;
+import entities.Task;
+import entities.TaskStatus;
 import entities.Comment;
 import repository.impl.CommentRepositoryImpl;
 
 import java.sql.Connection;
 
+
+import static db.connect.*;
+
 public class Main {
   public static void main(String[] args) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+
+            createTableIfNeeded(connection);
+
+            insertUser(connection, "Alice", "alice@example.com");
+            insertUser(connection, "Bob", "bob@example.com");
+            insertUser(connection, "robe", "bob2@example.com");
+            findUserByEmail(connection, "yera@gmail.com");
+            deleteUserById(connection, 2);
+            printAllUsers(connection);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
   }
+
 }
